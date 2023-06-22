070c32ea55b51c0d0ae88301b489b26133f2ad66
ohwphil <ohwphil@gmail.com>
2023-06-22 20:55:53 +0900

    feat: 캐시 로직 수정

--- src/pages/now-playing/index.js
+++ src/pages/now-playing/index.js
@@ -1,19 +1,12 @@
+// import { CacheUtils } from "apis/movieApi";
 import { CacheUtils } from "apis/movieApi";
 import MovieList from "components/movie-list";
-import { useQueryClient } from "react-query";
 
 const NowPlaying = () => {
-	const queryClient = useQueryClient();
-	const getNowPlayingMovie = () => {
-		const result = queryClient.getQueryData("get/now-playing");
-		return result || {};
-		// return { data, isLoading, isSuccess };
-		// You can use the cached data in other places as needed
-	};
-	CacheUtils.cacheNowPlayingMovie(1);
+	// CacheUtils.cacheNowPlayingMovie(1);
 
 	// 사용법
-	const nowPlayingMovies = getNowPlayingMovie().data;
+	const nowPlayingMovies = CacheUtils.cacheNowPlayingMovie().data.data;
 	console.log(nowPlayingMovies);
 	return nowPlayingMovies && <MovieList movies={nowPlayingMovies} />;
 };
--- src/pages/top-rated/index.js
+++ src/pages/top-rated/index.js
@@ -1,19 +1,17 @@
 import { CacheUtils } from "apis/movieApi";
 import MovieList from "components/movie-list";
-import { useQueryClient } from "react-query";
 
 const TopRated = () => {
-	const queryClient = useQueryClient();
 	const getTopRatedMovie = () => {
-		const result = queryClient.getQueryData("get/top-rated");
+		const result = queryClient.getQueryData(["get/top-rated"]);
 		return result || {};
 		// return { data, isLoading, isSuccess };
 		// You can use the cached data in other places as needed
 	};
-	CacheUtils.cacheTopRatedMovie(1);
+	// CacheUtils.cacheTopRatedMovie(1);
 
 	// 사용법
-	const topRatedMovies = getTopRatedMovie().data;
+	const topRatedMovies = CacheUtils.cacheTopRatedMovie().data.data;
 	console.log(topRatedMovies);
 	return topRatedMovies && <MovieList movies={topRatedMovies} />;
 };
