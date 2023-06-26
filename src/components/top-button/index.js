5d103c1d14fbc998548bb216b88afa2cba3f0f5d
zivivle <zivivle@gmail.com>
2023-06-26 20:32:32 +0900

    feat: 데이터 받아오는 로직에 최대 개수 60개로 수정하였습니다.

--- src/components/top-button/index.js
+++ src/components/top-button/index.js
@@ -25,7 +25,9 @@ const StyledTopButton = styled.button`
 	right: 20px;
 	z-index: 1000;
 	border: 1px solid black;
-	background-color: white;
+	background-color: #6739b6;
+	color: white;
+	font-weight: 900;
 	border-radius: 50%;
 	width: 50px;
 	height: 50px;
